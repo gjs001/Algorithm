de8cb46bee52a00c822cc1848a7a1f26031b60bd
GitHubgjs001 <GitHub gjs010@naver.com>
2017-01-05 17:42:37 +0900

    josephus 1
    using double list queue

--- ConsoleApplication2/ConsoleApplication2/소스.cpp
+++ ConsoleApplication2/ConsoleApplication2/소스.cpp
@@ -4,6 +4,10 @@
 
 
 using namespace std;
+
+int N, M;
+int ans[5001];
+
 class node{
 	friend class jose;
 
@@ -11,31 +15,76 @@ class node{
 	node *prev;
 	node *next;
 	int value;
+	int visited;
  public :
-	 node() { prev = this; next = this; value = NULL; }
+	 node() { prev = this; next = this; value = NULL; visited = 0; }
 
 	void attach(int data) {
 		node *addnode = new node;
-		addnode->next = this->next;
-		this->next->prev = addnode;
-		addnode->prev = this;
-		this->next = addnode;
 		addnode->value = data;
+		addnode->next = this;
+		this->prev->next = addnode;
+		addnode->prev = this->prev;
+		this->prev = addnode;
+
 	 }
-};
+	int getValue() {
 
-class jose {
-public:
-	int rear;
-	int front;
-	int count;
-
-	jose(int n) {
-		rear = front = 0;
-		count = n;
-		node *root = new node;
-		for (int i = 1; i <= n; i++) {
-			root->attach(i);
-		}
+		return value;
+	}
+	node* getNext() {
+		return next;
+	}
+	void setValue(int data) {
+		this->value = data;
+	}
+	int getVisited() {
+		return visited;
+	}
+	void setVisited() {
+		visited = 1;
 	}
 };
+
+void josephus(node *head) {
+	node *tmp = head;
+	int cnt = 0;
+
+	while (cnt < N) {
+		int move = 0;
+		while (move < M - 1) {
+			tmp = tmp->getNext();
+			if (!(tmp->getVisited())) move++;
+		}
+			ans[cnt] = tmp->getValue();
+			tmp->setVisited();
+			if (cnt != N - 1) {
+				tmp = tmp->getNext();
+				while (tmp->getVisited()) {
+					tmp = tmp->getNext();
+				}
+			}
+			
+		
+	cnt++;
+			
+
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	node a;
+	a.setValue(1);
+	for (int i = 1; i < N; i++) {
+		a.attach(i + 1);
+	}
+	josephus(&a);
+	cout << "<";
+		for (int i = 0; i < N -1 ; i++) {
+		cout << ans[i] <<", ";
+	}
+		cout << ans[N - 1] << ">\n";
+
+   
+}
