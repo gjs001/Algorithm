2bb735ed7320d732b0beaab8e9ae5ffc41a16efc
GitHubgjs001 <GitHub gjs010@naver.com>
2017-01-17 01:45:37 +0900

    algomaster-study week 1

---
+++ analysis_wordline/analysis_wordline/소스.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <iostream>
+#include <string>
+#include <cstring>
+
+
+using namespace std;
+
+char sArr[102];
+int main() {
+	
+
+
+	int c;
+	int small = 0;
+	int big = 0;
+	int num = 0;
+	int space = 0;
+	int len;
+
+	while(fgets(sArr, 102, stdin) != '\0') {
+
+		len = strlen(sArr);
+		for (int i = 0; i < len; i++) {
+			if ('a' <= sArr[i] && sArr[i] <= 'z') small++;
+			else if ('A' <= sArr[i] && sArr[i] <= 'Z') big++;
+			else if ('0' <= sArr[i] && sArr[i] <= '9') num++;
+			else if (' ' == sArr[i]) space++;
+		}
+
+		printf("%d %d %d %d\n", small, big, num, space);
+		small = big = num = space = 0;
+
+	}
+
+}
---
+++ kuk_young_su/kuk_young_su/소스.cpp
@@ -0,0 +1,53 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+typedef struct A{
+	char name[11]; int kuk; int young; int su; // string을 사용하지 않고 char 배열을 사용했을 때 소요시간 감소 (108ms -> 64ms)
+}A;
+
+
+
+
+bool cmp(const A &C1, const A &C2) {
+	if (C1.kuk != C2.kuk) return (C1.kuk > C2.kuk);
+	if (C1.young != C2.young) return (C1.young < C2.young);
+	if (C1.su != C2.su) return (C1.su > C2.su);
+	for (int i = 0; i < strlen(C1.name); i++) {
+		if (C1.name[i] != C2.name[i]) return C1.name[i] < C2.name[i];
+	} // = strcmp와 소요시간은 거의 같다.(백준 사이트 기준 동일시간)
+	
+}
+
+
+
+int main() {
+	int N;
+	cin >> N;
+	
+	A Clist[10000]; // vector 사용하지 않고 구조체 배열 선언 하였을 때 소요시간 감소(136ms -> 108ms)
+
+	for (int i = 0; i < N; i++) {
+	
+
+		 scanf("%s %d %d %d", Clist[i].name, &Clist[i].kuk, &Clist[i].young, &Clist[i].su);
+				
+	}
+
+
+
+	stable_sort(Clist, Clist+N, cmp);
+
+	for (int i = 0; i < N; i++) {
+		printf("%s\n", Clist[i].name);
+	}
+
+}
